dd54c86bc8ee5160330aa2826f44d452974527fe
Devexhhh <kumarsujal.codes@gmail.com>
2026-05-08 21:06:42 +0530

    added signup route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,29 @@
 import express from "express";
 import pg from "pg";
-import { HDNodeWallet, Wallet } from "ethers";
+import { HDNodeWallet } from "ethers";
+import { mnemonicToSeedSync, validateMnemonic } from "bip39";
+import { MNEMONICS } from "./config.js";
+
+const seed = mnemonicToSeedSync(MNEMONICS);
 
 const app = express();
+app.use(express.json());
 
 app.post("/signup", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
 
+    const userId = 1;
+    const hdNode = HDNodeWallet.fromSeed(seed);
+    const child = hdNode.derivePath(`m/44'/60'/${userId}'/0`);
+
+    console.log(child.privateKey);
+    console.log(child.address);
+
+    res.json({
+        userId
+    })
+    return child.privateKey;
 })
 
 app.get("/depositAddress/:userId", (req, res) => {
