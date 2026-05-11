2b2a90344c1ea062fa862b3e79636191bcbef638
Devexhhh <kumarsujal.codes@gmail.com>
2026-05-11 22:25:08 +0530

    modified with real-time block

---
+++ indexer/src/index.ts
@@ -0,0 +1,15 @@
+import { JsonRpcProvider } from "ethers";
+let CURRENT_BLOCK_NUMBER = 24936355;
+
+const provider = new JsonRpcProvider("https://eth-mainnet.g.alchemy.com/v2/dhuFXA2gtsJsGgz7diJSo")
+
+async function main() {
+    // get interested addresses from the DB
+    const interestedAddress = ["0x05557f7d084b12c0b303c03e6e4aed314696533b", "0x4666462aa29602bA4F0f576f8C2312D2818838E8", "0x4666462aa29602bA4F0f576f8C2312D2818838E8", "0x5CF00a901e91702Ad5E658C39a5A9F3A64fb4151"]
+    // inspect the block from native eth transactions on one of these addresses
+    const block = await provider.getBlock(CURRENT_BLOCK_NUMBER, true);
+    console.log(block?.transactions)
+    // Bad approach => Update the balance in the database
+}
+
+main();
