54b604f762d7824bb76c509d77ef4435f6c678ef
Devexhhh <kumarsujal.codes@gmail.com>
2026-05-11 22:07:13 +0530

    added index

---
+++ index.ts
@@ -0,0 +1,9 @@
+const CURRENT_BLOCK_NUMBER = 12313123;
+
+async function main() {
+    // get interested addresses from the DB
+
+    // inspect the block from native eth transactions on one of these addresses
+
+    // Bad approach => Update the balance in the database
+}
