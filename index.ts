72182442aea428a1e18b075d67aa61d686116804
Devexhhh <kumarsujal.codes@gmail.com>
2026-05-11 22:24:43 +0530

    added tsconfig

--- index.ts
+++
@@ -1,9 +0,0 @@
-let CURRENT_BLOCK_NUMBER = 12313123;
-
-async function main() {
-    // get interested addresses from the DB
-    const interestedAddress = ["0x4666462aa29602bA4F0f576f8C2312D2818838E8", "0x4666462aa29602bA4F0f576f8C2312D2818838E8", "0x5CF00a901e91702Ad5E658C39a5A9F3A64fb4151"]
-    // inspect the block from native eth transactions on one of these addresses
-
-    // Bad approach => Update the balance in the database
-}
